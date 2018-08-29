9e1dbcc00b0386b58bacce3603e28d55859909b2
Matheus Abrantes Cerqueira <matheus.abrantesc@gmail.com>
2018-08-29 15:28:50 -0300

    upgrade in the free pass

--- src/free_pass_node.cpp
+++ src/free_pass_node.cpp
@@ -8,86 +8,137 @@
 //for test
 #include "std_msgs/String.h"
 #include "std_msgs/Int8.h"
+#include "p2os_msgs/SonarArray.h"
 
 ros::Publisher pub;
+ros::Subscriber cloudSub;
+ros::Subscriber sonarSub;
+p2os_msgs::SonarArray sonar_array;
 
-void 
-cloud_cb (const sensor_msgs::PointCloud2ConstPtr& input)
-{
+int cloudFree,sonarFree,pathFree;
+
+void cloud_cb (const sensor_msgs::PointCloud2ConstPtr& input){
   // Create a container for the data.
   sensor_msgs::PointCloud2 output;
 
-  std_msgs::String msg;
-  std_msgs::Int8 free_pass_msg;
+  //std_msgs::Int8 free_pass_msg;
   std::stringstream ss;
 
-  int free_pass = 1,i;
+  int i;
 
   float limites[3] = {(0.7/2),-0.92,0.7};
-
-  //float minx= 1000,miny= 1000,minz = 1000;
-  //float maxx = -1000,maxy = -1000,maxz = -1000;
 
   pcl::PointCloud<pcl::PointXYZ> cloud1;
   pcl::fromROSMsg (*input, cloud1);
 
   int cloud_size =  cloud1.size();
 
+
+  cloudFree = 1;
+
   for (size_t i = 0; i < cloud_size; ++i)
   {
 
     if(cloud1[i].z < limites[2] && (abs(cloud1[i].x) < limites[0])){
-      free_pass = 0;
+      cloudFree = 0;
     }
-
-
-    /*if(cloud1[i].x < minx){
-      minx = cloud1[i].x;
-    }
-    if(cloud1[i].y < miny){
-      miny = cloud1[i].y;
-    }
-    if(cloud1[i].z < minz){
-      minz = cloud1[i].z;
-    }
-
-    if(cloud1[i].x > maxx){
-      maxx = cloud1[i].x;
-    }
-    if(cloud1[i].y > maxy){
-      maxy = cloud1[i].y;
-    }if(cloud1[i].z > maxz){
-      maxz = cloud1[i].z;
-    }*/
 
   }
 
-  ss << "FREE PASS: " <<free_pass;
+  ss << "FREE PASS: " <<cloudFree;
 
-  //ss << "min: "<<minx<<","<<miny<<","<<minz<<" Max: "<<maxx<<","<<maxy<<","<<maxz;
 
-  msg.data = ss.str();
 
   
 
   // Publish the data.
-  free_pass_msg.data = free_pass;
-  pub.publish (free_pass_msg);
+  //free_pass_msg.data = cloudFree;
+  //pub.publish (free_pass_msg);
 }
 
-int
-main (int argc, char** argv)
-{
+int check_pass_sonar(void){
+  
+  int N = 8, count =0, sonarFree=1;
+
+  float range,range2,threshold,threshold_minor = 0.25;
+  threshold = 0.5;
+
+  // do sonar 3 ao 6
+  for(count = 2;count<6;count++){
+    range = sonar_array.ranges[count];
+    if (range < threshold){
+    //se distancia[i] menor que limiar
+      sonarFree = 0;
+      return sonarFree;
+    }
+  }
+  
+  for(count = 0;count<2;count++){
+    range = sonar_array.ranges[count];
+
+    range2 = sonar_array.ranges[count + 6];
+
+    if (range < threshold_minor || range2 < threshold_minor){
+    //se distancia[i] menor que limiar
+      sonarFree = 0;
+      return sonarFree;
+    }
+  }
+
+}
+
+
+void sonar_cb(const p2os_msgs::SonarArray::ConstPtr& msg){
+
+  sonar_array = *msg;
+
+  sonarFree = check_pass_sonar();
+
+}
+
+
+int main (int argc, char** argv){
+
+  std_msgs::Int8 free_pass_msg;
+
+  int sonar_mode;
+
+
   // Initialize ROS
   ros::init (argc, argv, "free_pass_node");
   ros::NodeHandle nh;
 
+
+  nh.param<int>("sonar",sonar_mode,0);
+
+
   // Create a ROS subscriber for the input point cloud
-  ros::Subscriber sub = nh.subscribe ("input", 1, cloud_cb);
+  cloudSub = nh.subscribe ("input", 1, cloud_cb);
 
   // Create a ROS publisher for the output point cloud
   pub = nh.advertise<std_msgs::Int8> ("output", 1);
 
+  //subscriber para o sonar
+  sonarSub = nh.subscribe ("sonar",1,sonar_cb);
+
+
+  ros::Rate loop_rate(5);
+  while(ros::ok()){
+
+
+    free_pass_msg.data = cloudFree;
+    if (sonar_mode == 1){
+      free_pass_msg.data = cloudFree*sonarFree;
+    }
+
+
+    pub.publish (free_pass_msg);
+    
+    loop_rate.sleep();
+    ros::spinOnce();
+  }
+
+
   // Spin
-  ros::spin ();
+  //ros::spin ();
 }
